569e061d52f58fa2967620d59c96e1b448438e8a
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-10-29 23:33:13 +0700

    thêm package watch-video/

---
+++ NLUMovies_v2/react_native/screens/watch-video/index.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react"
+import { View, Text } from "react-native";
+import Video, { type VideoRef } from "react-native-video"
+
+export default function ViewVideo() {
+    const videoRef = useRef<VideoRef>(null);
+    return (
+        <View>
+            <Text>Hello?</Text>
+            <Video
+            source={{
+                uri: 'https://s5.phim1280.tv/20241028/cCmVbWnx/index.m3u8',
+                metadata: {
+                title: 'Custom Title',
+                subtitle: 'Custom Subtitle',
+                artist: 'Custom Artist',
+                description: 'Custom Description',
+                imageUri: 'https://pbs.twimg.com/profile_images/1498641868397191170/6qW2XkuI_400x400.png'
+                }
+            }}
+            controls
+            style={{ width: '100%', height: 200 }}
+            resizeMode="contain"
+            ref={videoRef}
+            />
+        </View>
+    )
+}
