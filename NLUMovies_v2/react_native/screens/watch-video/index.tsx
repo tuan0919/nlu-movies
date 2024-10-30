dd13268b056d7035adc68304b4b645fffe11337f
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-10-30 23:01:40 +0700

    Test tính năng phát video

--- NLUMovies_v2/react_native/screens/watch-video/index.tsx
+++ NLUMovies_v2/react_native/screens/watch-video/index.tsx
@@ -18,7 +18,19 @@ export default function ViewVideo() {
                 imageUri: 'https://pbs.twimg.com/profile_images/1498641868397191170/6qW2XkuI_400x400.png'
                 }
             }}
-            controls
+            bufferConfig={{
+                minBufferMs: 15000,
+                maxBufferMs: 50000,
+                bufferForPlaybackMs: 2500,
+                bufferForPlaybackAfterRebufferMs: 5000,
+                backBufferDurationMs: 120000,
+                cacheSizeMB: 0,
+                live: {
+                    targetOffsetMs: 500,
+                },
+              }}
+            controls={true}
+            fullscreenAutorotate={true}
             style={{ width: '100%', height: 200 }}
             resizeMode="contain"
             ref={videoRef}
