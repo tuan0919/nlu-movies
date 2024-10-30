aba9cacf3618d2e03b11d80817a88b1bc463dbdd
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-10-30 23:00:07 +0700

    chỉnh sửa navigationStack, thêm Navigation tab cho ứng dụng

--- NLUMovies_v2/App.tsx
+++ NLUMovies_v2/App.tsx
@@ -1,9 +1,42 @@
-import Navigation from './react_native/navigation';
 import React from 'react';
 import './global.css';
 import { verifyInstallation } from 'nativewind';
-
+import { NavigationContainer } from '@react-navigation/native';
+import HomeStack from './react_native/screens/navigation/navigation';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ViewVideo from './react_native/screens/watch-video';
+import { FilmIcon, HomeModernIcon } from 'react-native-heroicons/outline';
 export default function App() : React.JSX.Element {
+  const Tab = createBottomTabNavigator();
   verifyInstallation();
-  return <Navigation />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        headerShown: false,
+        tabBarActiveBackgroundColor: '#0a0a0a',
+        tabBarInactiveBackgroundColor: '#0a0a0a'
+      }}>
+        <Tab.Screen name="Home" options={
+          {
+            tabBarIcon: ({ color, size }) => <HomeModernIcon size={size} color={color} />,
+            tabBarActiveTintColor: '#28b463',
+            tabBarInactiveTintColor: 'gray',
+            tabBarLabelStyle: {
+              fontWeight: 800,
+            }
+          }
+        } component={HomeStack} />
+        <Tab.Screen name="My Video" 
+        options={{
+          tabBarIcon: ({ color, size }) => <FilmIcon size={size} color={color} />,
+          tabBarActiveTintColor: '#28b463',
+          tabBarInactiveTintColor: 'gray',
+          tabBarLabelStyle: {
+            fontWeight: 800,
+          }
+        }}
+        component={ViewVideo} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
