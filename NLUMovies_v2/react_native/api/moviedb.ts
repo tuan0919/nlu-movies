aebc841c7ad72c4ed39cccf50cf2dca784389205
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-02 23:10:23 +0700

    thêm một số endpoint mới cho api moviedb

--- NLUMovies_v2/react_native/api/moviedb.ts
+++ NLUMovies_v2/react_native/api/moviedb.ts
@@ -1,3 +1,9 @@
+import axios from "axios";
+import { ApplicationException } from "../exception/AppException";
+import type { TVSeriesDetails } from "../model/moviedb/TVSeriesDetails";
+import type { EpisodeDetails } from "../model/moviedb/EpisodeDetails";
+import type { Season } from "../model/moviedb/TVSeriesSeason";
+
 const API_KEY = "db55323b8d3e4154498498a75642b381";
 // Endpoints
 const apiBaseURL = "https://api.themoviedb.org/3";
@@ -20,3 +26,32 @@ export const fallbackMoviesPoster =
   "https://www.kindpng.com/picc/m/736-7369205_play-button-png-pic-video-default-transparent-png.png";
 export const fallbackPersonImage =
   "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
+
+async function _apiCall <T>  (endpoint: string, params?: any): Promise<T> {
+    const options = {
+        method: 'GET',
+        url: endpoint,
+        params: params ? params : {},
+    };
+    try {
+        return axios.request<T>(options).then(value => value.data);
+    } catch (error) {
+        throw new ApplicationException({
+            message: `Lỗi khi fetch: ${endpoint}`,
+            code: 999,
+            statusCode: 400,
+        });
+    }
+}
+
+export async function loadTVSeriesDetails(tvId: number): Promise<TVSeriesDetails> {
+  return _apiCall(`${apiBaseURL}/tv/${tvId}?api_key=${API_KEY}`) 
+}
+
+export async function loadTVEpisodeDetails(tvId: string, season: number, episode: number): Promise<EpisodeDetails> {
+  return _apiCall(`${apiBaseURL}/tv/${tvId}/season/${season}/episode/${episode}?api_key=${API_KEY}`) 
+}
+
+export async function loadSeasonDetails(tvId: number, season: number): Promise<Season> {
+  return _apiCall(`${apiBaseURL}/tv/${tvId}/season/${season}?api_key=${API_KEY}`) 
+}
