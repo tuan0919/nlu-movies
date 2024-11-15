cd5a57c61d29753b815eb79f91e10caf87559cc6
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-15 17:50:10 +0700

    Sử dụng lại màn hình chính của ứng dụng thay vì màn hình xem video để test trước đó

--- NLUMovies_v2/App.tsx
+++ NLUMovies_v2/App.tsx
@@ -6,37 +6,45 @@ import HomeStack from './react_native/screens/navigation/navigation';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ViewVideo from './react_native/screens/watch-video';
 import { FilmIcon, HomeModernIcon } from 'react-native-heroicons/outline';
-export default function App() : React.JSX.Element {
+import { Text } from 'react-native';
+
+function Home () {
   const Tab = createBottomTabNavigator();
-  verifyInstallation();
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        headerShown: false,
-        tabBarActiveBackgroundColor: '#0a0a0a',
-        tabBarInactiveBackgroundColor: '#0a0a0a'
-      }}>
-        <Tab.Screen name="Home" options={
-          {
-            tabBarIcon: ({ color, size }) => <HomeModernIcon size={size} color={color} />,
-            tabBarActiveTintColor: '#28b463',
-            tabBarInactiveTintColor: 'gray',
-            tabBarLabelStyle: {
-              fontWeight: 800,
-            }
-          }
-        } component={HomeStack} />
-        <Tab.Screen name="My Video" 
-        options={{
-          tabBarIcon: ({ color, size }) => <FilmIcon size={size} color={color} />,
+    <Tab.Navigator screenOptions={{
+      headerShown: false,
+      tabBarActiveBackgroundColor: '#0a0a0a',
+      tabBarInactiveBackgroundColor: '#0a0a0a'
+    }}>
+      <Tab.Screen name="Home" options={
+        {
+          tabBarIcon: ({ color, size }) => <HomeModernIcon size={size} color={color} />,
           tabBarActiveTintColor: '#28b463',
           tabBarInactiveTintColor: 'gray',
           tabBarLabelStyle: {
             fontWeight: 800,
           }
-        }}
-        component={ViewVideo} />
-      </Tab.Navigator>
-    </NavigationContainer>
+        }
+      } component={HomeStack} />
+      <Tab.Screen name="My Video" 
+      options={{
+        tabBarIcon: ({ color, size }) => <FilmIcon size={size} color={color} />,
+        tabBarActiveTintColor: '#28b463',
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabelStyle: {
+          fontWeight: 800,
+        }
+      }}
+      component={ViewVideo} />
+    </Tab.Navigator>
+  </NavigationContainer>
+  )
+}
+
+export default function App() : React.JSX.Element {
+  verifyInstallation();
+  return (
+    <Home/>
   )
 }
