d1c57d05dbcba1ca9214765befd2d38658ad3be8
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-15 17:49:24 +0700

    thêm file index để expose import cho các package liên quan

---
+++ NLUMovies_v2/react_native/components/index.ts
@@ -0,0 +1,12 @@
+import Cast from "./Cast";
+import Loading from "./Loading";
+import MovieList from "./MovieList";
+import PressIcon from "./PressIcon";
+import TrendingMovies from "./TrendingMovies";
+export {
+    Cast,
+    Loading,
+    MovieList,
+    PressIcon,
+    TrendingMovies
+}
---
+++ NLUMovies_v2/react_native/repositories/index.ts
@@ -0,0 +1,9 @@
+import CastRepository from "./CastRepository"
+import IMDBRepository from "./imdbRepository"
+import MovieRepository from "./MovieRepository"
+
+export {
+    CastRepository,
+    IMDBRepository,
+    MovieRepository,
+}
